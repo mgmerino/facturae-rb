f00f79b4e95f6dc0437c2e5c667a741ab8952ed3
Manuel González <mgmerino@gmail.com>
2025-04-03 15:57:31 +0200

    Include unit of measure

--- spec/lib/facturae/models/line_spec.rb
+++ spec/lib/facturae/models/line_spec.rb
@@ -2,14 +2,17 @@
 
 module Facturae
   RSpec.describe Line do
+    let(:line) do
+      described_class.new(item_description: "Item description",
+                          quantity: 1,
+                          unit_price_without_tax: 10.0,
+                          gross_amount: 10.0,
+                          total_cost: 10.0,
+                          unit_of_measure: "01")
+    end
+
     describe "#initialize" do
       it "initializes all properties with expected values" do
-        line = described_class.new(item_description: "Item description",
-                                   quantity: 1,
-                                   unit_price_without_tax: 10.0,
-                                   gross_amount: 10.0,
-                                   total_cost: 10.0)
-
         expect(line.item_description).to eq("Item description")
         expect(line.quantity).to eq(1)
         expect(line.unit_price_without_tax).to eq(10.0)
@@ -20,23 +23,30 @@ module Facturae
 
     describe "#valid?" do
       context "when the line is valid" do
+        let(:line) do
+          described_class.new(item_description: "Item description",
+                              quantity: 1,
+                              unit_price_without_tax: 10.0,
+                              gross_amount: 10.0,
+                              total_cost: 10.0,
+                              unit_of_measure: "01")
+        end
+
         it "returns true" do
-          line = described_class.new(item_description: "Item description",
-                                     quantity: 1,
-                                     unit_price_without_tax: 10.0,
-                                     gross_amount: 10.0,
-                                     total_cost: 10.0)
           expect(line.valid?).to be(true)
         end
       end
 
       context "when the line is not valid" do
+        let(:line) do
+          described_class.new(item_description: nil,
+                              quantity: 1,
+                              unit_price_without_tax: 10.0,
+                              gross_amount: 10.0,
+                              total_cost: 10.0,
+                              unit_of_measure: "01")
+        end
         it "returns false" do
-          line = described_class.new(item_description: nil,
-                                     quantity: 1,
-                                     unit_price_without_tax: 10.0,
-                                     gross_amount: 10.0,
-                                     total_cost: 10.0)
           expect(line.valid?).to be(false)
         end
       end
