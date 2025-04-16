c1f8f3c819731f01ad3571cccfe6ed37310e13e1
Manuel González <mgmerino@gmail.com>
2025-04-16 17:28:02 +0200

    refactor: break down in methods

--- lib/facturae/xades/object_info.rb
+++ lib/facturae/xades/object_info.rb
@@ -2,22 +2,26 @@
 
 module Facturae
   module Xades
+    # TODO: review static values
+    # This class is responsible for building the XAdES ObjectInfo element.
     class ObjectInfo
       include Utils
 
-      def initialize(doc, signature_id, sp_id, object_id, ref_doc_id, certificate, options)
+      DIGEST_METHOD_ALGORITHM = "http://www.w3.org/2001/04/xmlenc#sha512"
+
+      def initialize(doc, ids, certificate, options)
         @doc = doc
-        @signature_id = signature_id
-        @sp_id = sp_id
-        @object_id = object_id
-        @ref_doc_id = ref_doc_id
+        @signature_id = ids[:signature_id]
+        @sp_id = ids[:sp_id]
+        @object_id = ids[:object_id]
+        @ref_doc_id = ids[:ref_doc_id]
         @certificate = certificate
         @options = options
       end
 
       def build
-        object_node = @doc.create_element("ds:Object")
-        object_node["Id"] = @object_id
+        main_node = @doc.create_element("ds:Object")
+        main_node["Id"] = @object_id
 
         qualifying_props_node = @doc.create_element("xades:QualifyingProperties")
         qualifying_props_node["Target"] = "##{@signature_id}"
@@ -25,116 +29,128 @@ module Facturae
         signed_properties_node = @doc.create_element("xades:SignedProperties")
         signed_properties_node["Id"] = @sp_id
 
-        # SignedSignatureProperties
-        signed_properties_node.add_child(build_signed_signature_properties)
-
-        # SignedDataObjectProperties
-        signed_properties_node.add_child(build_signed_data_object_properties)
+        signed_properties_node.add_child(build_signed_signature_properties_node)
+        signed_properties_node.add_child(build_signed_data_object_properties_node)
 
         qualifying_props_node.add_child(signed_properties_node)
-        object_node.add_child(qualifying_props_node)
-        object_node
+        main_node.add_child(qualifying_props_node)
+
+        main_node
       end
 
       private
 
-      def build_signed_signature_properties
-        signed_signature_props_node = @doc.create_element("xades:SignedSignatureProperties")
-        # <xades:SigningTime>
-        signed_signature_props_node.add_child(@doc.create_element("xades:SigningTime", Time.now.utc.iso8601))
-
+      def build_signed_signature_properties_node
+        main_node = @doc.create_element("xades:SignedSignatureProperties")
+        main_node.add_child(@doc.create_element("xades:SigningTime", Time.now.utc.iso8601))
         # <xades:SigningCertificate>
-        signed_signature_props_node.add_child(build_signing_certificate_node)
-
+        main_node.add_child(build_signing_certificate_node)
         # <xades:SignaturePolicyIdentifier>
-        signed_signature_props_node.add_child(build_signature_policy_identifier_node)
-
+        main_node.add_child(build_signature_policy_identifier_node)
         # <xades:SignerRole>
-        signed_signature_props_node.add_child(build_signer_role_node)
+        main_node.add_child(build_signer_role_node)
 
-        signed_signature_props_node
+        main_node
       end
 
       def build_signing_certificate_node
-        signing_certificate_node = @doc.create_element("xades:SigningCertificate")
+        main_node = @doc.create_element("xades:SigningCertificate")
         cert_node = @doc.create_element("xades:Cert")
         cert_node.add_child(build_cert_digest_node)
         cert_node.add_child(build_issuer_serial_node)
 
-        signing_certificate_node.add_child(cert_node)
+        main_node.add_child(cert_node)
 
-        signing_certificate_node
+        main_node
       end
 
       def build_cert_digest_node
-        cert_digest_node = @doc.create_element("xades:CertDigest")
+        main_node = @doc.create_element("xades:CertDigest")
         digest_method_node = @doc.create_element("ds:DigestMethod")
         digest_method_node["Algorithm"] = "http://www.w3.org/2001/04/xmlenc#sha512"
-        cert_digest_node.add_child(digest_method_node)
+        main_node.add_child(digest_method_node)
 
         digest_val = Base64.strict_encode64(OpenSSL::Digest::SHA512.digest(@certificate.to_der))
         digest_val_node = @doc.create_element("ds:DigestValue", digest_val)
-        cert_digest_node.add_child(digest_val_node)
+        main_node.add_child(digest_val_node)
 
-        cert_digest_node
+        main_node
       end
 
       def build_issuer_serial_node
-        is_node = @doc.create_element("xades:IssuerSerial")
+        main_node = @doc.create_element("xades:IssuerSerial")
         xname   = @doc.create_element("ds:X509IssuerName", @certificate.issuer.to_s)
         xserial = @doc.create_element("ds:X509SerialNumber", @certificate.serial.to_s)
-        is_node.add_child(xname)
-        is_node.add_child(xserial)
+        main_node.add_child(xname)
+        main_node.add_child(xserial)
 
-        is_node
+        main_node
       end
 
       def build_signature_policy_identifier_node
-        spi = @doc.create_element("xades:SignaturePolicyIdentifier")
-        spid = @doc.create_element("xades:SignaturePolicyId")
+        main_node = @doc.create_element("xades:SignaturePolicyIdentifier")
 
-        pid = @doc.create_element("xades:SigPolicyId")
-        pid.add_child(@doc.create_element("xades:Identifier",
-                                          "http://www.facturae.es/politica_de_firma_formato_facturae/politica_de_firma_formato_facturae_v3_1.pdf"))
-        pid.add_child(@doc.create_element("xades:Description", "Política de Firma FacturaE v3.1"))
-        spid.add_child(pid)
+        signature_policy_id_node = @doc.create_element("xades:SignaturePolicyId")
+        signature_policy_id_node.add_child(build_sig_policy_id_node)
+        signature_policy_id_node.add_child(build_sig_policy_hash_node)
 
-        sph = @doc.create_element("xades:SigPolicyHash")
-        dm = @doc.create_element("ds:DigestMethod")
-        dm["Algorithm"] = "http://www.w3.org/2000/09/xmldsig#sha1"
-        sph.add_child(dm)
-        dv = @doc.create_element("ds:DigestValue", "Ohixl6upD6av8N7pEvDABhEL6hM=")
-        sph.add_child(dv)
-        spid.add_child(sph)
+        main_node.add_child(signature_policy_id_node)
 
-        spi.add_child(spid)
-        spi
+        main_node
+      end
+
+      def build_sig_policy_id_node
+        main_node = @doc.create_element("xades:SigPolicyId")
+        main_node.add_child(@doc.create_element("xades:Identifier",
+                                                "http://www.facturae.es/politica_de_firma_formato_facturae/politica_de_firma_formato_facturae_v3_1.pdf"))
+        main_node.add_child(@doc.create_element("xades:Description", "Política de Firma FacturaE v3.1"))
+
+        main_node
+      end
+
+      def build_sig_policy_hash_node
+        main_node = @doc.create_element("xades:SigPolicyHash")
+
+        digest_method_node = @doc.create_element("ds:DigestMethod")
+        digest_method_node["Algorithm"] = "http://www.w3.org/2000/09/xmldsig#sha1"
+        main_node.add_child(digest_method_node)
+
+        digest_value_node = @doc.create_element("ds:DigestValue", "foo")
+        main_node.add_child(digest_value_node)
+
+        main_node
       end
 
       def build_signer_role_node
-        sr = @doc.create_element("xades:SignerRole")
-        cr = @doc.create_element("xades:ClaimedRoles")
-        cr.add_child(@doc.create_element("xades:ClaimedRole", "emisor")) # Ejemplo
-        sr.add_child(cr)
-        sr
+        main_node = @doc.create_element("xades:SignerRole")
+        claimed_roles_node = @doc.create_element("xades:ClaimedRoles")
+        claimed_roles_node.add_child(@doc.create_element("xades:ClaimedRole", "emisor"))
+        main_node.add_child(claimed_roles_node)
+
+        main_node
       end
 
-      def build_signed_data_object_properties
-        sdp = @doc.create_element("xades:SignedDataObjectProperties")
+      def build_signed_data_object_properties_node
+        main_node = @doc.create_element("xades:SignedDataObjectProperties")
+        main_node.add_child(build_data_object_format_node)
 
-        dof = @doc.create_element("xades:DataObjectFormat")
-        dof["ObjectReference"] = "##{@ref_doc_id}"
+        main_node
+      end
 
-        dof.add_child(@doc.create_element("xades:Description", "Factura electrónica"))
-        oid = @doc.create_element("xades:ObjectIdentifier")
-        ident = @doc.create_element("xades:Identifier", "urn:oid:1.2.840.10003.5.109.10")
-        ident["Qualifier"] = "OIDAsURN"
-        oid.add_child(ident)
-        dof.add_child(oid)
+      def build_data_object_format_node
+        main_node = @doc.create_element("xades:DataObjectFormat")
+        main_node["ObjectReference"] = "##{@ref_doc_id}"
 
-        dof.add_child(@doc.create_element("xades:MimeType", "text/xml"))
-        sdp.add_child(dof)
-        sdp
+        main_node.add_child(@doc.create_element("xades:Description", "Factura electrónica"))
+        object_id_node = @doc.create_element("xades:ObjectIdentifier")
+        identifier_node = @doc.create_element("xades:Identifier", "urn:oid:1.2.840.10003.5.109.10")
+        identifier_node["Qualifier"] = "OIDAsURN"
+        object_id_node.add_child(identifier_node)
+        main_node.add_child(object_id_node)
+
+        main_node.add_child(@doc.create_element("xades:MimeType", "text/xml"))
+
+        main_node
       end
     end
   end
--- spec/lib/facturae/xades/object_info_spec.rb
+++ spec/lib/facturae/xades/object_info_spec.rb
@@ -14,13 +14,17 @@ module Facturae
       end
 
       let(:xml_doc) { Nokogiri::XML(xml) }
-      let(:signature_id) { "Signature#{}" }
-      let(:sp_id) { "SignedProperties-ID-#{}" }
-      let(:object_id) { "Object-ID-#{}" }
-      let(:ref_doc_id) { "Reference-ID-#{}" }
+      let(:ids) do
+        {
+          signature_id: "Signature",
+          sp_id: "SignedProperties-ID-",
+          object_id: "Object-ID-",
+          ref_doc_id: "Reference-ID-"
+        }
+      end
       let(:certificate) { OpenSSL::X509::Certificate.new(File.read("spec/fixtures/certificate.pem")) }
       let(:options) { {} }
-      let(:object_info) { ObjectInfo.new(xml_doc, signature_id, sp_id, object_id, ref_doc_id, certificate, options) }
+      let(:object_info) { ObjectInfo.new(xml_doc, ids, certificate, options) }
 
       describe "#build" do
         before do
@@ -28,14 +32,43 @@ module Facturae
         end
 
         it "builds the XadesObject element with the correct structure" do
-          # signature_node = xml_doc.at_xpath(".//ds:Signature")
-          # result = signature_node.add_child(object_info.build)
           result = xml_doc.root.add_child(object_info.build)
 
           expect(result.name).to eq("Object")
-          expect(result["Id"]).to eq(object_id)
-          expect(result.at_xpath(".//xades:QualifyingProperties")["Target"]).to eq("##{signature_id}")
-          expect(result.at_xpath(".//xades:QualifyingProperties/xades:SignedProperties")["Id"]).to eq(sp_id)
+          expect(result["Id"]).to eq(ids[:object_id])
+          expect(result.at_xpath(".//xades:QualifyingProperties")["Target"]).to eq("##{ids[:signature_id]}")
+          expect(result.at_xpath(".//xades:QualifyingProperties/xades:SignedProperties")["Id"]).to eq(ids[:sp_id])
+        end
+
+        it "builds the SignedSignatureProperties element with the correct structure" do
+          result = xml_doc.root.add_child(object_info.build)
+          signed_signature_properties = result.at_xpath(".//xades:SignedSignatureProperties")
+
+          expect(signed_signature_properties).not_to be_nil
+          expect(signed_signature_properties.at_xpath(".//xades:SigningTime").text)
+            .to match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/)
+          expect(signed_signature_properties.at_xpath(".//xades:SigningCertificate")).not_to be_nil
+          expect(signed_signature_properties.at_xpath(".//xades:SignaturePolicyIdentifier")).not_to be_nil
+          expect(signed_signature_properties.at_xpath(".//xades:SignerRole")).not_to be_nil
+        end
+
+        it "builds the SigningCertificate element with the correct structure" do
+          result = xml_doc.root.add_child(object_info.build)
+          signing_certificate = result.at_xpath(".//xades:SigningCertificate")
+
+          expect(signing_certificate).not_to be_nil
+          cert_node = signing_certificate.at_xpath(".//xades:Cert")
+          expect(cert_node).not_to be_nil
+          expect(cert_node.at_xpath(".//xades:CertDigest")).not_to be_nil
+          expect(cert_node.at_xpath(".//xades:IssuerSerial")).not_to be_nil
+        end
+
+        it "builds the CertDigest element with the correct structure" do
+          result = xml_doc.root.add_child(object_info.build)
+          cert_digest = result.at_xpath(".//xades:CertDigest")
+
+          expect(cert_digest).not_to be_nil
+          expect(cert_digest.at_xpath(".//ds:DigestMethod")["Algorithm"]).to eq("http://www.w3.org/2001/04/xmlenc#sha512")
         end
       end
     end
